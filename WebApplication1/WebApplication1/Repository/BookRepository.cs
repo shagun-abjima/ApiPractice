05708816b455192b14bd92290e7a3ecf82fa2340
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-03-12 18:04:17 +0530

    initial commiit

---
+++ WebApplication1/WebApplication1/Repository/BookRepository.cs
@@ -0,0 +1,91 @@
+﻿using Azure;
+using BookStoreApi.Data;
+using BookStoreApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreContext _context;
+        
+
+        public async Task<List<BookModel>> GetAllBooksAsync()
+        {
+            var records = await _context.Books.ToListAsync();
+           
+        }
+
+        public async Task<BookModel> GetBookByIdAsync(int bookId)
+        {
+            var records = await _context.Books.Where(x => x.Id == bookId).Select(x => new BookModel()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description
+            }).FirstOrDefaultAsync();
+
+            return records;
+
+            var book = await _context.Books.FindAsync(bookId);
+            
+        }
+
+        public async Task<int> AddBookAsync(BookModel bookModel)
+        {
+            var book = new Books()
+            {
+                Title = bookModel.Title,
+                Description = bookModel.Description
+            };
+
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return book.Id;
+        }
+
+        public async Task UpdateBookAsync(int bookId, BookModel bookModel)
+        {
+            //var book = await _context.Books.FindAsync(bookId);
+            //if (book != null)
+            //{
+            //    book.Title = bookModel.Title;
+            //    book.Description = bookModel.Description;
+
+            //    await _context.SaveChangesAsync();
+            //}
+
+            var book = new Books()
+            {
+                Id = bookId,
+                Title = bookModel.Title,
+                Description = bookModel.Description
+            };
+
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookPatchAsync(int bookId, JsonPatchDocument<> bookModel)
+        {
+            var book = await _context.Books.FindAsync(bookId);
+            if (book != null)
+            {
+                bookModel. (book);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteBookAsync(int bookId)
+        {
+            var book = new Books() { Id = bookId };
+
+            _context.Books.Remove(book);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ WebApplication1/WebApplication1/Repository/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace BookStoreApi.Repository
+{
+    public interface IBookRepository
+    {
+        Task<List<BookModel>> GetAllBooksAsync();
+        Task<BookModel> GetBookByIdAsync(int bookId);
+        Task<int> AddBookAsync(BookModel bookModel);
+        Task UpdateBookAsync(int bookId, BookModel bookModel);
+        Task UpdateBookPatchAsync(int bookId, JsonPatchDocument bookModel);
+        Task DeleteBookAsync(int bookId);
+    }
+}
